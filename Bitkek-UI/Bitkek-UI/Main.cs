f272b7b17ecb8650ac0735f185aa9580c1d5961c
YC0de360 <bubla.babla@web.de>
2017-04-11 19:25:42 +0200

    added UI classes

---
+++ Bitkek-UI/Bitkek-UI/Main.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bitkek_UI
+{
+    public class main
+    {
+        static int Main(String[] args) {
+            return -1;
+        }
+    }
+}
